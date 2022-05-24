9d5fad141e731de1425efd5adae437302b984a2d
DAMILARE1012 <dammy4did@gmail.com>
2022-05-24 14:03:58 +0100

    Fourth Commit

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,35 +6,43 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    public function home(){
-        return view('pages.home');
+    public function home()
+    {
+        return view('pages.front.home');
     }
 
-    public function about_us(){
-        return view('pages.about');
+    public function about_us()
+    {
+        return view('pages.front.about');
     }
 
-    public function veterinarian(){
-        return view('pages.vet');
+    public function veterinarian()
+    {
+        return view('pages.front.vet');
     }
 
-    public function gallery(){
-        return view('pages.gallery');
+    public function gallery()
+    {
+        return view('pages.front.gallery');
     }
 
-    public function services(){
-        return view('pages.services');
+    public function services()
+    {
+        return view('pages.front.services');
     }
 
-    public function blog(){
-        return view('pages.blog');
+    public function blog()
+    {
+        return view('pages.front.blog');
     }
 
-    public function contact(){
-        return view('pages.contact');
+    public function contact()
+    {
+        return view('pages.front.contact');
     }
 
-    public function pricing(){
-        return view('pages.pricing');
+    public function pricing()
+    {
+        return view('pages.front.pricing');
     }
 }
